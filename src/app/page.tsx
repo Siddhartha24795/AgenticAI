cd226338b68555e620499f305fb52bd051a44d01
Siddhartha Mishra <SIDDHARTHAMISHRA24795@gmail.com>
2025-07-19 10:11:07 +0000

    Still .env seems empty to me, diagnose option is not clickable after upl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-import Header from '@/components/layout/Header';
 import HomeComponent from '@/components/sections/Home';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow container mx-auto p-4 sm:p-6 md:p-8">
         <HomeComponent />
-      </main>
-    </div>
   );
 }
